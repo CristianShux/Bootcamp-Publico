2b3d09d064878836f82c9849e0111b22af156b55
Cristian Jurajuria <cljurajuria23@gmail.com>
2025-09-27 23:55:50 -0300

    primera tarea core de proyecto en react con vite

---
+++ STACK-MERN/REACT/TareasCore/PrimerProyectoReact/vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1>¡Bienvenido a mi aplicación de React!</h1>
+        <h2>Lista de cosas por hacer:</h2>
+        <ul>
+          <li>Aprender React</li>
+          <li>Practicar con Vite</li>
+          <li>Construir proyectos increíbles</li>
+        </ul>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
